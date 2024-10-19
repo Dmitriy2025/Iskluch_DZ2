4b7e83d15756c688ad658d7a23be4bfefa994b05
Dmitriy <dmitriykharitonov333@gmail.com>
2024-10-19 12:57:38 +0300

    Сделал 1 задание

---
+++ main/java/ex1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class ex1 {
+    public static void main(String[] args) {
+        System.out.println("Ваше число: " + newNumber());
+    }
+
+    public static float newNumber () {
+        Scanner scanner = new Scanner(System.in);
+        float number = 0;
+        boolean validInput = false;
+
+        while (!validInput) {
+            try {
+                System.out.print("Введи дробное число: ");
+                number = scanner.nextFloat();
+                validInput = true;
+            } catch (InputMismatchException e) {
+                System.out.println("Введено неверное число. Попробуйте еще раз.");
+                scanner.next();
+            }
+        }
+        return number;
+    }
+}
